2437aa9b9f3ee9472c70bf74dff94df34af930a0
Marius Ceobanu <ceobanu.marius@gmail.com>
2021-04-15 10:48:12 +0300

    Implement Header layout component and start Home page

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,37 @@
+import React from "react";
+import classNames from "classnames";
+import { makeStyles } from "@material-ui/core/styles";
+
+import styles from "../../jss/landingPageStyle";
+
+import Header from "../Layout/Header";
+import CustomButton from "../Layout/CustomButton";
+import HeaderLinks from "../Layout/HeaderLinks";
+
+const dashboardRoutes = [];
+
+const useStyles = makeStyles(styles);
+
+function HomePage(props) {
+    const classes = useStyles();
+    const { ...rest } = props;
+
+    return (
+        <div>
+            <Header
+                color="transparent"
+                routes={dashboardRoutes}
+                brand="VI - LA"
+                rightLinks={<HeaderLinks />}
+                fixed
+                changeColorOnScroll={{
+                    height: 400,
+                    color: "white"
+                }}
+                { ...rest }
+            />
+        </div>
+    )
+}
+
+export default HomePage;
